5190e4de843720a547db7a8c83032a0a71b811aa
Rubén Segura <rsegrom@gmail.com>
2019-08-02 17:51:41 +0200

    Gen componentes app, creado routing, añadido animate

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroe/:id', component: HeroeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'heroes' }
+];
+
+@NgModule({
+  imports: [ 
+    RouterModule.forRoot( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
