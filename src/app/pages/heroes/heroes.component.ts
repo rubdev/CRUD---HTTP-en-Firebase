5dbed09c979a5d2c84ee33aa18f1277d1d8d35e6
Rubén Segura <rsegrom@gmail.com>
2019-08-13 12:50:25 +0200

    Muestra mensaje de cargando o no hay registros

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -11,13 +11,16 @@ import Swal from 'sweetalert2';
 export class HeroesComponent implements OnInit {
 
   heroes: HeroeModel[] = [];
+  cargando = false;
 
   constructor( private heroesService: HeroesService ) { }
 
   ngOnInit() {
+    this.cargando = true;
     this.heroesService.getHeroes()
                       .subscribe( respuesta => {
                         this.heroes = respuesta;
+                        this.cargando = false;
                       });
   }
 
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HeroeModel } from '../models/heroe.model';
-import { map } from 'rxjs/operators';
+import { map, delay } from 'rxjs/operators';
 
 
 @Injectable({
@@ -38,7 +38,8 @@ export class HeroesService {
   getHeroes() {
     return this.http.get(`${ this.url }/heroes.json`)
                     .pipe(
-                      map( respuesta => this.toArray( respuesta ) )
+                      map( respuesta => this.toArray( respuesta ) ),
+                      delay( 1500 )
                     );
   }
 
